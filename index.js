be5f79390d0e679772f56fcfdec574cb2970446c
Shirley Zhang <shirleyz9402@yahoo.com>
2017-12-28 03:51:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,9 +11,10 @@ var array = []
 
 function johnLennonFacts(facts) {
 var shoutFacts = []
-let i=0 
+let i = 0
  while (i<facts.length)  {
     shoutFacts.push(`${facts[i]}!!!`);
-  }
+  i++
+ }
   return shoutFacts;
 }
